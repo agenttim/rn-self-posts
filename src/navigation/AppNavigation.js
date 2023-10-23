9127ecdb892b7c47290d17f67cc3cd5ad2364d16
agent_tim <agent_tim@mail.ru>
2023-10-23 16:30:31 +0500

    добавили навигацию

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,7 +1,9 @@
-import {createAppContainer} from 'react-navigation'
+import {createAppContainer, ThemeColors} from 'react-navigation'
 import {createStackNavigator} from 'react-navigation-stack'
 import {MainScreen} from "../screens/MainScreen";
 import {PostScreen} from "../screens/PostScreen";
+import {THEME} from "../theme";
+import {Platform} from "react-native";
 
 let PostNavigator = createStackNavigator({
         Main: MainScreen,
@@ -10,7 +12,13 @@ let PostNavigator = createStackNavigator({
         }
     },
     {
-        initialRouteName: 'Main'
+        initialRouteName: 'Main',
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff'
+            },
+            headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR
+        }
     }
 )
 
