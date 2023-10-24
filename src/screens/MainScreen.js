3366569bb1b263dd188c10dc63226fb9bd0f1b6d
agent_tim <agent_tim@mail.ru>
2023-10-24 10:01:35 +0500

    отображение постов на главной странице

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, FlatList} from 'react-native';
+import {DATA} from "../data";
+import {Post} from "../components/Post";
 
 export const MainScreen = ({navigation}) => {
     const goToPost = () => {
@@ -8,11 +10,16 @@ export const MainScreen = ({navigation}) => {
     }
 
     return (
-        <View style={styles.center}>
-            <Text>MainScreen</Text>
-            <Button title="Go to Post" onPress={goToPost} />
-        </View>
-    )
+        <View style={styles.wrapper}>
+            <FlatList
+                data={DATA}
+                keyExtractor={post => post.id.toString()}
+                renderItem={({item}) => {
+                    return <Post post={item}/>
+                }
+                }
+            />
+        </View>)
 }
 
 MainScreen.navigationOptions = {
@@ -24,5 +31,8 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
+    },
+    wrapper: {
+        padding: 10
     }
 })
